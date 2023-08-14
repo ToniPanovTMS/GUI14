a2af4c50f1ce0f95f9bad77a467cf7a7defffc9e
ToniPanovTMS <-l>
2023-08-14 18:28:01 +0300

    good

--- src/GUI14.java
+++ src/GUI14.java
@@ -6,7 +6,9 @@ class GUI14 {
         JFrame jFrame=getFrame();
         JPanel jPanel =new JPanel();
         jFrame.add(jPanel);
+        Font font = new Font("Ani",Font.BOLD,50);
         JLabel jLabel= new JLabel("Моя первая надпись!");
+        jLabel.setFont(font);
         jPanel.add(jLabel);
     }
     static JFrame getFrame(){
@@ -14,7 +16,7 @@ class GUI14 {
         jFrame.setTitle("GUI14");
         jFrame.setVisible(true);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        jFrame.setBounds(dim.width / 2 - 300 / 2, dim.height / 2 - 150 / 2, 300, 150);
+        jFrame.setBounds(dim.width / 2 - 300 / 2, dim.height / 2 - 150 / 2, 600, 200);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         return jFrame;
     }
